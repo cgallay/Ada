7a4c6ffaeeab62e82a3549971faf825821c2a222
topali2 <thomas.batschelet@epfl.ch>
2017-12-18 00:33:36 +0100

    deezer done and added function to merge all previews

--- Project/helpers.py
+++ Project/helpers.py
@@ -1,9 +1,37 @@
 from tqdm import tqdm
 import requests
 import re
+from spotify import *
 
 topic2genre = {0:'Raggae', 1:'Pop', 2:'Jazz', 3:'Rock', 4:'Blues', 5:'Latin', 6:'Electronic', 7:'Country', 8:'Metal', 9:'Rap'}
 
+
+def add_previews_spotify(df, path=None):
+
+    spot = spotify()
+    df_previews = df.copy()
+    df_previews['preview_url'] = 0
+    tracks = {}
+
+    for ind in tqdm(range(0, len(df_previews))):
+        track = df_previews.loc[ind, ['artist_name', 'title', 'track_id']]
+        try:
+            tracks[track[2]] = spot.search_preview(track[0], track[1])
+        except Exception:
+            print(track)
+
+    df_previews = df_previews.set_index('track_id')
+
+    for key in tqdm(tracks.keys()):
+
+        df_previews.loc[key, 'preview_url'] = tracks[key]
+
+    df_previews = df_previews.reset_index()
+
+    if (path):
+        df_previews.to_pickle(path)
+
+    return df_previews
 
 def add_previews_deezer(df, path=None):
 
@@ -50,6 +78,17 @@ def search_deezer(artist, title):
 
     return res_track[0]['preview']
 
+def complete_previews(df_deezzer, df_spotify, path=None):
+    df_deezzer = df_deezzer[[x is None for x in df_deezzer.preview_url]].set_index('track_id')
+    df_spotify = df_spotify.set_index('track_id')
+    added = 0
+    for x in df_deezzer.index:
+        extra = df_spotify.loc[x, 'preview_url']
+        if (extra is not None):
+            df_deezzer.loc[x, 'preview_url'] = extra
+            added += 1
 
-
-
+    if(path):
+        df_deezzer.to_pickle(path)
+    print(added, ' entries were added')
+    return  df_deezzer
