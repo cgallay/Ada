2c2ea53ee0147a42774ad96d4d06f68e367acfef
Charles Gallay <charles.gallay@epfl.ch>
2017-12-19 19:06:30 +0100

    Charles: Update website

--- docs/js/help.js
+++ docs/js/help.js
@@ -5,11 +5,29 @@ audio = null;
 myPlot.on('plotly_click', function(data){
     console.log(data);
     //alert(data.points[0].customdata)
-    if(audio){
-      audio.pause()
+    if(data.points[0].customdata) {
+      if(audio){
+        audio.pause()
+      }
+      audio = new Audio(data.points[0].customdata);
+      audio.play();
+      container = document.getElementById('history');
+      container.insertBefore(document.createElement('br'), container.childNodes[0])
+      container.insertBefore(document.createTextNode(data.points[0].text.replace(/<br>/g, ' ')), container.childNodes[0])
+    } else {
+      showToast();
     }
-    audio = new Audio(data.points[0].customdata);
-    audio.play();
-    //container = document.getElementById('selection');
-    //container.appendChild(document.createTextNode(data.points[0].text));
 });
+
+function showToast() {
+  var x = document.getElementById("snackbar")
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+function pause() {
+  if(audio){
+    audio.pause()
+  }
+}
